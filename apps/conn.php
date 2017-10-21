ef609452581f91f8fb2e564aeafc52c428d25aea
dxz2002 <daidou@daidou.pw>
2017-10-21 17:40:32 +0800

    update conn.php

--- apps/conn.php
+++ apps/conn.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_server_name='localhost';
-$mysql_username='root';
-$mysql_password='root';
-$mysql_database='daidr';
+$mysql_server_name='mysql.coding.io';
+$mysql_username='user-fsRmGdMovo';
+$mysql_password='_s0-$,gQ~]F(X)[Y@eEB';
+$mysql_database='db-rgE4nohPON';
 $conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting");
 mysql_query("set names 'utf8'");
 mysql_select_db($mysql_database);
