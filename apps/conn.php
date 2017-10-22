f40a132b88be6b2ec5d1710e9a504234c71a71c1
戴兜 <daidou@daidou.pw>
2017-10-22 16:23:05 +0800

    更换mysql方式

--- apps/conn.php
+++ apps/conn.php
@@ -3,7 +3,6 @@ $mysql_server_name='mysql.coding.io';
 $mysql_username='user-C0YUg9csPs';
 $mysql_password='!;)t{-x_L``G0S-p3ZcR';
 $mysql_database='db-vDwMVW9bzh';
-$conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting");
-mysql_query("set names 'utf8'");
-mysql_select_db($mysql_database);
+$conn=mysqli_connect($mysql_server_name,$mysql_username,$mysql_password,$mysql_database) or die("error connecting");
+mysqli_query("set names 'utf8'");
 ?>
