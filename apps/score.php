6e516a3e5096a37947f7d13fc8fc114a78a143d8
戴兜 <daidou@daidou.pw>
2017-10-22 15:12:41 +0800

    恢复原post方式

--- apps/score.php
+++ apps/score.php
@@ -1,25 +1,9 @@
 <?php
-error_reporting(0);
 	$uid = $_POST['id'];
+	$data = 'secret=s9xlSSrsOjtBgd8kPT7comyg&sitekey=DjrcXUshI3GJuza0LmsY9rz2&name='.$uid;
+	$url = "https://api.ppoi.org/user/balance";
 
-	$data = array(
-    'secret'=>'s9xlSSrsOjtBgd8kPT7comyg&sitekey=DjrcXUshI3GJuza0LmsY9rz2',
-    'name'=>$uid
-    ); 
-
-	$query = http_build_query($data); 
-
-	$options['http'] = array(
-		 'timeout'=>60,
-		 'method' => 'POST',
-		 'header' => 'Content-type:application/x-www-form-urlencoded',
-		 'content' => $query
-		);
-
-	$url = "http://api.ppoi.org/user/balance";
-	$context = stream_context_create($options);
-	$res = file_get_contents($url, false, $context);
-
+	$res = http_request($url, $data);
 	if ($res == "") {
 		echo "error";
 		exit;
@@ -31,5 +15,21 @@ error_reporting(0);
 		echo (string)$score;
 	} else {
 		echo "0";
+	}
+
+   function http_request($url, $data = null)
+	{
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+		if (!empty($data)){
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		}
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		$output = curl_exec($curl);
+		curl_close($curl);
+		return $output;
 	}
 ?>
