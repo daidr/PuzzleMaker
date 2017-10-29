d2a995feec551156a027a6334d463461d717c744
戴兜 <daidou@daidou.pw>
2017-10-29 22:48:17 +0800

    更新登录

--- apps/showuser.php
+++ apps/showuser.php
@@ -5,7 +5,7 @@
 
 	$account = $_COOKIE['access_token'];
 	$uid = $_POST['uid'];
-	$url = "https://api.weibo.com/2/account/get_uid.json?access_token=".$account."&uid=".$uid;
+	$url = "https://api.weibo.com/2/users/show.json?access_token=".$account."&uid=".$uid;
 	$res = Requests::get($url);
 	$res = $res->body;
 	echo $res;
