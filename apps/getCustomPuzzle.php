01125bda8308408eeac9a44323e11689b77473de
戴兜 <daidou@daidou.pw>
2017-11-25 22:21:54 +0800

    更新接口

--- apps/getCustomPuzzle.php
+++ apps/getCustomPuzzle.php
@@ -1,13 +1,13 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 include('Requests-1.7.0/library/Requests.php');
 Requests::register_autoloader();
 require("conn.php");
 $type = $_GET['type'];
-$uid = $_COOKIE["cqpm_uid"];
+$uid = $_COOKIE["uid"];
 $account = $_COOKIE["access_token"];
 
-if($type == "" || $uid == ""){echo "console.log(\"error1\");";mysqli_close($conn);exit;}
+if($type == "" || $uid == ""){echo "console.log(\"error\");";mysqli_close($conn);exit;}
 
 
 //$url = "https://api.weibo.com/2/account/get_uid.json?access_token=2.00n345vFHekr5E20a7291eed0JDp21";
@@ -17,17 +17,17 @@ $res = $res->body;
 
 $json=json_decode($res);
 if ($res == "") {
-	echo "console.log(\"error2\");";
+	echo "console.log(\"error\");";
 	mysqli_close($conn);
 	exit;
 }
 
 if ($json->error) {
-	echo "console.log(\"error3\");";
+	echo "console.log(\"error\");";
 	mysqli_close($conn);
 	exit;
 } else {
-	if($json->uid <> $uid){echo "console.log(\"error4\");";mysqli_close($conn);exit;}
+	if($json->uid <> $uid){echo "console.log(\"error\");";mysqli_close($conn);exit;}
 }
 
 if($type == "getScript"){
@@ -45,12 +45,16 @@ function getCustomPuzzleID(){
 	while($row = mysqli_fetch_row($result)){
 		$IDList = $row[0];
 		if($IDList == ""){
-			echo "empty";
 			mysqli_free_result($result);
 			mysqli_close($conn);
+			return "empty";
 			exit;
+		} else {
+			mysqli_free_result($result);
+			mysqli_close($conn);
+			return $IDList;
 		}
-		return $IDList;
+		
 	}
 }
 
