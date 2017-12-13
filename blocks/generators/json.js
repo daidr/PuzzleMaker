87e092aec535cc7fd3e992d41f6675d47510bbe0
戴兜 <daidou@daidou.pw>
2017-12-13 21:56:25 +0800

    更新json分类

---
+++ blocks/generators/json.js
@@ -0,0 +1,56 @@
+'use strict';
+
+goog.provide('Blockly.JavaScript.json');
+
+goog.require('Blockly.JavaScript');
+
+Blockly.JavaScript.json_createJson = function(b) {
+	var a = "{}";
+	return [a, Blockly.JavaScript.ORDER_ATOMIC]
+};
+
+Blockly.JavaScript.json_getLength = function(c) {
+	var a = Blockly.JavaScript.valueToCode(c, "NAME", Blockly.JavaScript.ORDER_ATOMIC);
+	var b = "Object.keys(" + a + ").length";
+	return [b, Blockly.JavaScript.ORDER_ATOMIC]
+};
+
+Blockly.JavaScript.json_setKey = function(e) {
+	var d = Blockly.JavaScript.valueToCode(e, "VAR", Blockly.JavaScript.ORDER_ATOMIC);
+	var a = Blockly.JavaScript.valueToCode(e, "propertyName", Blockly.JavaScript.ORDER_ATOMIC);
+	var c = Blockly.JavaScript.valueToCode(e, "valueName", Blockly.JavaScript.ORDER_ATOMIC);
+	var b = d + "[" + a + "] = " + c + ";\n";
+	return b
+};
+
+Blockly.JavaScript.json_deleteKey = function(d) {
+	var c = Blockly.JavaScript.valueToCode(d, "VAR", Blockly.JavaScript.ORDER_ATOMIC);
+	var a = Blockly.JavaScript.valueToCode(d, "propertyName", Blockly.JavaScript.ORDER_ATOMIC);
+	var b = "delete " + c + "[" + a + "];\n";
+	return b
+};
+
+Blockly.JavaScript.json_getKey = function(d) {
+	var c = Blockly.JavaScript.valueToCode(d, "VAR", Blockly.JavaScript.ORDER_ATOMIC);
+	var a = Blockly.JavaScript.valueToCode(d, "propertyName", Blockly.JavaScript.ORDER_ATOMIC);
+	var b = c + "[" + a + "]";
+	return [b, Blockly.JavaScript.ORDER_ATOMIC]
+};
+
+Blockly.JavaScript.json_array2json_str = function(d) {
+	var c = Blockly.JavaScript.valueToCode(d, "text", Blockly.JavaScript.ORDER_ATOMIC);
+	if (c == "" || c == null) {
+		c = "[]"
+	}
+	var b = "JSON.stringify(" + c + ")";
+	return [b, Blockly.JavaScript.ORDER_ATOMIC]
+};
+
+Blockly.JavaScript.json_array2json_obj = function(d) {
+	var c = Blockly.JavaScript.valueToCode(d, "text", Blockly.JavaScript.ORDER_ATOMIC);
+	if (c == "" || c == null) {
+		c = "[]"
+	}
+	var b = "JSON.parse(" + c + ")";
+	return [b, Blockly.JavaScript.ORDER_ATOMIC]
+};
---
+++ blocks/json.js
@@ -0,0 +1,103 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.json');  // Deprecated
+goog.provide('Blockly.Constants.json');
+
+goog.require('Blockly.Blocks');
+
+Blockly.Constants.json.HUE = Blockly.Msg["JSON_HUE"];
+Blockly.Blocks.json.HUE = Blockly.Constants.json.HUE;
+
+Blockly.Blocks.json_getKey = {
+	init: function() {
+		this.appendDummyInput().appendField("取出json");
+		this.appendValueInput("VAR");
+		this.appendValueInput("propertyName").setCheck("String").appendField("中");
+		this.appendDummyInput().appendField("的值");
+		this.setInputsInline(true);
+		this.setOutput(true);
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("获取键值");
+		this.setHelpUrl(null);
+		this.contextMenuMsg_ = Blockly.Msg.VARIABLES_GET_CREATE_SET
+	}
+};
+
+Blockly.Blocks.json_deleteKey = {
+	init: function() {
+		this.appendDummyInput().appendField("删除json");
+		this.appendValueInput("VAR");
+		this.appendValueInput("propertyName").setCheck("String").appendField("中  键值（key）为");
+		this.appendDummyInput().appendField("的记录");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("删除键值");
+		this.setHelpUrl(null);
+		this.contextMenuMsg_ = Blockly.Msg.VARIABLES_SET_CREATE_GET
+	}
+};
+
+Blockly.Blocks.json_setKey = {
+	init: function() {
+		this.appendDummyInput().appendField("设置json");
+		this.appendValueInput("VAR");
+		this.appendValueInput("propertyName").setCheck("String").appendField("中");
+		this.appendDummyInput().appendField("的值");
+		this.appendValueInput("valueName").appendField("等于");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("设置键值");
+		this.setHelpUrl(null);
+		this.contextMenuMsg_ = Blockly.Msg.VARIABLES_SET_CREATE_GET
+	}
+};
+
+
+Blockly.Blocks.json_getLength = {
+	init: function() {
+		this.appendValueInput("NAME").setCheck("json").appendField("json");
+		this.appendDummyInput().appendField("的长度");
+		this.setInputsInline(true);
+		this.setOutput(true, "Number");
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("返回json的长度");
+		this.setHelpUrl(null)
+	}
+};
+
+Blockly.Blocks.json_createJson = {
+	init: function() {
+		this.appendDummyInput().appendField("创建一个空json");
+		this.setInputsInline(true);
+		this.setOutput(true,"json");
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("创建一个空json");
+		this.setHelpUrl(null)
+	}
+};
+
+Blockly.Blocks.json_array2json_str = {
+	init: function() {
+		this.appendValueInput("text").setCheck("Array").appendField("将列表");
+		this.appendDummyInput().appendField("转换为json文本");
+		this.setOutput(true, "String");
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("把列表解析为json文本");
+		this.setHelpUrl(null)
+	}
+};
+
+Blockly.Blocks.json_array2json_obj = {
+	init: function() {
+		this.appendValueInput("text").setCheck("Array").appendField("将json文本");
+		this.appendDummyInput().appendField("转换为json对象");
+		this.setOutput(true, "json");
+		this.setColour(Blockly.Blocks.json.HUE);
+		this.setTooltip("把列表解析为json对象");
+		this.setHelpUrl(null)
+	}
+};
